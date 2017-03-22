57817747e71e7008d41fdf1093718e27e22dcd75
David Frese <david.frese@active-group.de>
2017-03-22 23:41:51 +0100

    Remove vdom creation from patch benchmarks.

--- bench/dfrese/orpheus/patch_bench.cljs
+++ bench/dfrese/orpheus/patch_bench.cljs
@@ -19,50 +19,57 @@
   (let [[node state] (t/prepare!)]
     (render-seq state node s)))
 
-(defbench patch-random-bench
-  (let [N 1000
+(def rand-60 (random-props 60))
+(def rand-30 (random-props 30))
+(def rand-20 (random-props 20))
 
-        e1 (random-props 30)
-        e2 (random-props 20)]
-    ;; a3be45c90e43680246622ab7a62707418901315a: 2267.5000132   ~2ms per patch
-    ;; no lifecycle: 2149.7340216 ms  -5%
-    ;; new diff-path: ~1100 ms
-    (bench-seq (interleave (repeat N e1) (repeat N e2)))))
+(defbench no-change-identical-bench
+  (let [N 100000]
+    (bench-seq (repeat N rand-60))))
+
+(def no-change-seq
+  (let [N 3000
+        e #(random-props 30)]
+    (mapv e (range 0 N))))
+(defbench no-change-equal-bench
+  (bench-seq no-change-seq))
+
+(defbench patch-random-bench
+  (let [N 1000]
+    (bench-seq (interleave (repeat N rand-30) (repeat N rand-20)))))
+
+(def long-l {"childNodes" (vec (repeat 100 (html/div)))})
+(def empty-l {"childNodes" []})
 
 (defbench create-rows-bench
-  (let [N 50 ;; repetitions
-        M 1000 ;; size
-        long {"childNodes" (vec (repeat M (html/div)))}
-        empty {"childNodes" []}]
-    (bench-seq (interleave (repeat N long) (repeat N empty)))))
+  (let [N 2000]
+    (bench-seq (interleave (repeat N long-l) (repeat N empty-l)))))
+
+(def divs {"childNodes" (repeat 1000 (html/div))})
+(def spans {"childNodes" (repeat 1000 (html/span))})
 
 (defbench replace-rows-bench
-  (let [N 20 ;; repetitions
-        M 1000 ;; size
-        divs {"childNodes" (repeat M (html/div))}
-        spans {"childNodes" (repeat M (html/span))}]
+  (let [N 30]
     (bench-seq (interleave (repeat N divs) (repeat N spans)))))
 
+(def part-1 {"childNodes" (vec (repeat 1000 (html/div "A")))})
+(def part-2 {"childNodes" (vec (apply concat (repeat 100 (concat (repeat 9 (html/div "A"))
+                                                                 [(html/div "B")]))))})
+
 (defbench partial-update-bench
-  (let [N 20    ;; repetitions
-        M 1000 ;; size
-        l1 {"childNodes" (repeat M (html/div "A"))}
-        l2 {"childNodes" (apply concat (repeat (/ M 10) (concat (repeat 9 (html/div "A"))
-                                                                [(html/div "B")])))}]
-    (bench-seq (interleave (repeat N l1) (repeat N l2)))))
+  (let [N 20]
+    (bench-seq (interleave (repeat N part-1) (repeat N part-2)))))
+
+(def unselected (html/div {:style {"background-color" "transparent"}}))
+(def selected (html/div {:style {"background-color" "grey"}}))
+(def sel-1 {"childNodes" (vec (repeat 1000 unselected))})
+(def sel-2 {"childNodes" (vec (concat (repeat (dec 499) unselected)
+                                      [selected]
+                                      (repeat 500 unselected)))})
 
 (defbench select-row-bench
-  (let [N 25 ;; repetitions
-        M 1000 ;; size
-
-        unselected (html/div {:style {"background-color" "transparent"}})
-        selected (html/div {:style {"background-color" "grey"}})
-        
-        l1 {"childNodes" (repeat M unselected)}
-        l2 {"childNodes" (concat (repeat (dec (/ M 2)) unselected)
-                                 [selected]
-                                 (repeat (/ M 2) unselected))}]
-    (bench-seq (interleave (repeat N l1) (repeat N l2)))))
+  (let [N 25]
+    (bench-seq (interleave (repeat N sel-1) (repeat N sel-2)))))
 
 ;; swap-rows
 ;; remove-row
